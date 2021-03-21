17489d3f702bfdb7f6432e5fa216434e649ea0d5
Brian Clayton <finalb@projectfinalboss.com>
2021-03-21 16:48:07 -0400

    update code comment out old code.

--- test.cpp
+++ test.cpp
@@ -9,53 +9,67 @@
 using namespace std;
 
 
+void StoryText()
+{
+    cout <<"You find yourself trapped in a well secured cement basement with 3 other captives." << endl;
+    cout <<"You all don't know how you ended up here, but you do know that there is a way out." << endl;
+    cout <<"By the door is a security padlock with a keypad." << endl;
+    cout <<"On the wall to your left is a message stating that you must guess the right passcode." << endl;
+    cout <<"If you guess right, the door will open to your safety, " << endl;
+    cout <<"if you guess wrong, then toxic gas will be released in the room leading you and your fellow captives to doom." << endl;
+    cout <<"Since no one in the room decided to take responsibility for their lives by trying to figure out the code, you all decided to play rock paper scissors." << endl;
+    cout <<"It turns out that you are the one to try and open the door.  You kept in mind the hint at the end of the message on the wall:" << endl;
+    cout <<"'There are three numbers needed to open the door, three of the numbers multiplied together make n," << endl;
+    cout <<"the numbers added together makes n.'" << endl;
+}
+
 void play_game()
 {
-    int random = rand() % 251;
-    cout<<"Guess a number: ";
-    while(true)
-    {
-        int guess;
-        cin>>guess;
-        if(guess == random)
-        {
-            cout<<"You win!\n";
-            break;
-        }else if (guess < random)
-        {
-            cout<<"Too low \n";
-        }else
-        {
-            cout<<"Too High\n";
-        }
-    }
+    // int random = rand() % 251;
+    // cout<<"Guess a number: ";
+    // while(true)
+    // {
+    //     int guess;
+    //     cin>>guess;
+    //     if(guess == random)
+    //     {
+    //         cout<<"You win!\n";
+    //         break;
+    //     }else if (guess < random)
+    //     {
+    //         cout<<"Too low \n";
+    //     }else
+    //     {
+    //         cout<<"Too High\n";
+    //     }
+    // }
 }
 
 int main()
 {
-        srand(time(NULL));
-        int choice;
+    //     srand(time(NULL));
+    //     int choice;
 
     
-    do
-    {
-        cout<<"0.Quit"; 
-    cout<< endl;
-    cout<<"1.play game\n";
-    cin >> choice;
+    // do
+    // {
+    //     cout<<"0.Quit"; 
+    // cout<< endl;
+    // cout<<"1.play game\n";
+    // cin >> choice;
     
-    switch(choice)
-    {
-        case 0:
-        cout<< "Thanks for nothing\n";
-        return 0;
-        case 1:
-        play_game();
-        break;
+    // switch(choice)
+    // {
+    //     case 0:
+    //     cout<< "Thanks for nothing\n";
+    //     return 0;
+    //     case 1:
+    //     play_game();
+    //     break;
         
-    }
+    // }
 
-    }while(choice != 0);
+    // }while(choice != 0);
     
     
     
